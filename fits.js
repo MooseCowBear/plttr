98ff84d7ff8413373b52ac8976527b2753fc6942
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-01 07:46:46 -0400

    Add functions for SVD

--- fits.js
+++ fits.js
@@ -1,4 +1,348 @@
+//functions for linear fits.
 
+function SVD(m, n, eps, tol, inputMatrix) {
+  /*
+    SVD algorithm from Golub and Reinsch 1970, 
+    except that goto statements have been replaced by functions. 
+    outputs SVD matrices q, u, v where q is a triangular matrix represented as an array.
+    leaves the input matrix unaltered. 
+  */
+	const u = Array(m).fill().map(() => Array(n).fill(0.0)); 
+	const v = Array(n).fill().map(() => Array(n).fill(0.0));
+	const q = Array(n).fill(0.0);
+
+	const e = Array(n).fill(0.0);
+
+	for (let i = 0; i < m; i ++) {
+		for (let j = 0; j < n; j++) {
+			u[i][j] = inputMatrix[i][j]; 
+		}
+	}
+
+	let l, s, f, h, y; 
+
+	let g = x = 0;
+	
+	for (let i = 0; i < n; i ++) {
+		e[i] = g;
+		s = 0; 
+		l = i + 1
+		for (let j = i; j < m; j ++) {
+			s += u[j][i]**2;
+		}
+		if (s < tol) {
+			g = 0;
+		}
+		else {
+			f = u[i][i];
+			if (f < 0) {
+				g = Math.sqrt(s);
+			}
+			else {
+				 g = -Math.sqrt(s);
+			}
+
+			h = f * g - s;
+			u[i][i] = f - g;
+
+			for (let j = l; j < n; j ++) {
+				s = 0;
+				for (let k = i; k < m; k ++) {
+					s += u[k][i] * u[k][j];
+				}
+
+				f = s / h;
+
+				for (let k = i; k < m; k++) {
+					u[k][j] += f * u[k][i];
+				}
+			}
+		}
+		q[i] = g;
+		s = 0;
+
+		for (let j = l; j < n; j ++) {
+			s += u[i][j]**2;
+		}
+		if (s < tol) {
+			g = 0;
+		}
+		else {
+			f = u[i][i + 1];
+			if (f < 0) {
+				g = Math.sqrt(s);
+			}
+			else {
+				g = -Math.sqrt(s);
+			}
+			h = f * g - s;
+			u[i][i + 1] = f - g;
+
+			for (let j = l; j < n; j ++) {
+				e[j] = u[i][j] / h;
+			}
+			for (let j = l; j < m; j ++) {
+				s = 0;
+				for (let k = l; k < n; k ++) {
+					s += u[j][k] * u[i][k];
+				}
+				for (let k = l; k < n; k ++) {
+					u[j][k] += s * e[k];
+				}
+			}
+		}
+
+		y = Math.abs(q[i]) + Math.abs(e[i]);
+		if (y > x) {
+			x = y;
+		}
+	} //end for i
+
+	//accumulation of right-hand transformations
+	for (let i = n - 1; i > -1; i --) {
+		if (g !== 0) {
+			h = u[i][i + 1] * g;
+			for (let j = l; j < n; j ++) {
+				v[j][i] = u[i][j] / h;
+			}
+			for (let j = l; j < n; j++) {
+				s = 0;
+				for (let k = l; k < n; k ++) {
+					s += u[i][k] * v[k][j];
+				}
+				for (let k = l; k < n; k ++) {
+					v[k][j] += s * v[k][i];
+				}
+			}
+		}
+		for (let j = l; j < n; j ++) {
+			v[i][j] = 0;
+			v[j][i] = 0;
+		}
+		v[i][i] = 1;
+		g = e[i];
+		l = i;
+	}
+
+	//accumulation of left-hand transformations
+	for (let i = n - 1; i > -1; i --) {
+		l = i + 1;
+		g = q[i];
+
+		for (let j = l; j < n; j++) {
+			u[i][j] = 0;
+		}
+		if (g !== 0) {
+			h = u[i][i] * g;
+			for (let j = l; j < n; j ++) {
+				s = 0;
+				for (let k = l; k < m; k ++) {
+					s += u[k][i] * u[k][j];
+				}
+				f = s / h;
+				for (let k = i; k < m; k ++) {
+					u[k][j] += f * u[k][i];
+				}
+			}
+			for(let j = i; j < m; j ++) {
+				u[j][i] /= g;
+			}
+		}
+		else {
+			for (let j = i; j < m; j ++) {
+				u[j][i] = 0;
+			}
+		}
+		u[i][i] += 1;
+	}
+
+	//diagonalization of the bidiagonal form
+	eps *= x;
+	let z = 0;
+	for (let k = n - 1; k > -1; k --) {
+		let its = 0;
+		let cont = true;
+		let converged = false;
+
+		while(cont) {
+			[its, converged, z] = testFsplitting(k, e, u, eps, q, m, n, v, its, z);
+			if (its > 30 || converged === true) {
+				cont = false;
+			}
+		}
+	}
+	return [q, u, v]; //arr that can be unpacked with {u, v, w} = SVD(m, n, eps, tol, inputMatrix);
+}
+
+function testFsplitting(k, e, u, eps, q, m, n, v, its, z) {
+	its += 1;
+	let L = 0;
+	let converged = false;
+
+	for (let l = k; l > -1; l --) {
+		L = l;
+		if (Math.abs(e[l]) <= eps) {
+			[z, converged] = testFconvergence(k, L, q);
+			if (converged) {
+				convergence(z, n, k, v, q);
+				return [its, true, z];
+			}
+		}
+		if (Math.abs(q[l - 1]) <= eps) {
+			if (L > 0) {
+				[z, converged] = cancellation(L, eps, e, u, q, m, k);
+				if (converged) {
+					convergence(z, n, k, v, q);
+					return [its, true, z];
+				}
+			}
+			else {
+				[z, converged] = testFconvergence(k, L, q);
+				if (converged) {
+					convergence(z, n, k, v, q);
+					return [its, true, z];
+				}
+			}
+		}	
+	}
+	if (L > 0) {
+		[z, converged] = cancellation(L, eps, e, u, q, m, k);
+		if (converged) {
+			convergence(z, n, k, v, q);
+			return [its, true, z];
+		}
+	}
+	else {
+		[z, converged] = testFconvergence(k, L, q);
+		if (converged) {
+			convergence(z, n, k, v, q);
+			return [its, true, z];
+		}
+	}
+	z = QRtransformation(m, n, q, e, k, L, z, v, u);
+	return [its, false, z];
+}
+
+function QRtransformation(m, n, q, e, k, l, z, v, u) {
+  /* 
+    plus shift from bottom 2x2 minor. 
+    only returning z, since all other (non-arr, non-matrix) values will be overwritten.
+    the original paper gave options for whether u, v matrices would be returned. 
+    here, we choose to always return them. 
+  */
+	let x = q[l]; 
+	let y = q[k - 1];
+	let g = e[k - 1];
+	let h = e[k];
+	let f = ((y - z) * (y + z) + (g - h) * (g + h)) / (2 * h * y);
+
+	g = Math.sqrt(f * f + 1);
+	let interm = 0;
+	if (f < 0) {
+		interm = f - g;
+	}
+	else {
+		interm = f + g;
+	}
+	f = ((x - z) * (x + z) + h * (y/interm - h)) / x;
+
+	let c = 1.0
+	let s = 1.0;
+
+	for (let i = l + 1; i < k + 1; i ++) {
+    //won't be here if k == l
+		g = e[i];
+		y = q[i];
+		h = s * g;
+		g = c * g;
+		z = Math.sqrt(f * f + h * h);
+		e[i - 1] = z;
+		c = f / z;
+		s = h / z;
+		f = x * c + g * s;
+		g = -x * s + g * c;
+		h = y * s;
+		y *= c;
+		for (let j = 0; j < n; j ++) { //skip this if you don't care about v
+			x = v[j][i - 1];
+			z = v[j][i];
+			v[j][i - 1] = x * c + z * s;
+			v[j][i] = -x * s + z * c;
+		}
+		z = Math.sqrt(f * f + h * h);
+		q[i - 1] = z;
+		c = f / z;
+		s = h / z;
+		f = c * g + s * y;
+		x = -s * g + c * y;
+		for (let j = 0; j < m; j ++) { //skip this if you don't care about u
+			y = u[j][i - 1];
+			z = u[j][i];
+			u[j][i - 1] = y * c + z * s;
+			u[j][i] = -y * s + z * c;
+		}
+	}
+	e[l] = 0;
+	e[k] = f;
+	q[k] = x;
+
+	return z;
+}
+
+function convergence(z, n, k, v, q) {
+	if (z < 0) {
+		q[k] = -z;
+		for (let j = 0; j < n; j ++) {
+			v[j][k] = -v[j][k];
+		}
+	}
+}
+
+function testFconvergence(k, l, q) {
+	let converged = false;
+	z = q[k];
+	if (l === k) {
+		converged = true;
+	}
+	return [z, converged];
+}
+
+function cancellation(l, eps, e, u, q, m, k) {
+	let c = z = 0;
+	let s = 1.0;
+	let ll = l - 1; 
+	let converged = false;
+
+	let f, g, h, y;
+
+	for (let i = l; i < k + 1; i ++) {
+		f = s * e[i];
+		e[i] *= c;
+		if (Math.abs(f) <= eps) {
+			[z, converged] = testFconvergence(k, l, q);
+			if (converged) {
+				return [z, converged];
+			}
+		}
+		else {
+			g = q[i];
+			q[i] = Math.sqrt(f * f + g * g);
+			h = q[i];
+			c = g / h;
+			s = -f / h;
+			for (let j = 0; j < m; j ++) {
+				y = u[j][ll];
+				z = u[j][i];
+				u[j][ll] = y * c + z * s;
+				u[j][i] = -y * s + z * c;
+			}
+		}
+	}
+	[z, converged] = testFconvergence(k, l, q); 
+	return [z, converged];
+}
+
+//functions for the non-linear fits. i.e. exponential and power law.
 
 function nonlinearFit(xs, ys, fit) {
 
@@ -339,7 +683,7 @@ function funcsNonlinear(xi, a) { //as in nPlot
 	const dyda =  Array(5).fill(0.0);
 	
 	dyda[0] = xi**a[1]; 
-  
+
 	if (xi > 0) {
 		dyda[1] = a[0] * (xi**a[1]) * Math.log(xi);
 	}
