ed422ed25027b81a6a0eaf8f6fe13f0a6b9f9a10
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-01 13:43:00 -0400

    Refactor solve for y and compute rmse functions to use switch

--- plot.js
+++ plot.js
@@ -1,5 +1,6 @@
 // functions for the graph and graph report
 
+//except for title, will stay the same
 let layout = {
   title: "", //THIS WILL CHANGE = eq from reportRMSE??
   showlegend: false, 
@@ -12,93 +13,98 @@ let layout = {
   } 
 };
 
-//THIS AND RMSE CAN BE SWITCH STATEMENTS
-//solving for y, for plotting regression line
+/*
+  going to need up to 3 plots: active points, inactive points, regression line. 
+  want functions to return the jsons that are passed to the plot function.
+  plus a plot function. 
+*/
+
 function solveForY(xs, fit, coefs) {
 	const xsToGraph = [];
 	const ysToGraph = [];
 
-	if (fit === "quadratic") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[0]*xs[i]**2 + coefs[1]*xs[i] + coefs[2]);
-		}
-	}
-	else if (fit === "linear") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[0]*xs[i] + coefs[1]);
-		}
-	}
-	else if (fit === "square law") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[0]*xs[i]**2);
-		}
-	}
-	else if (fit === "inverse") {
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] !== 0) {
-				xsToGraph.push(xs[i]);
-				ysToGraph.push(coefs[0]*(1/xs[i])); 
-			}
-		}
-	}
-	else if (fit === "inverse square") {
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] !== 0) {
-				xsToGraph.push(xs[i]);
-				ysToGraph.push(coefs[0]*(1/xs[i]**2)); 
-			}
-		}
-	}
-	else if (fit === "proportional") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[0]*xs[i]);
-		}
-	}
-	else if (fit === "square root") {
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] >= 0) {
-				xsToGraph.push(xs[i]);
-				ysToGraph.push(coefs[0]*Math.sqrt(xs[i]));
-			}
-		}
-	}
-	else if (fit === "exactly proportional") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(xs[i]);
-		}
-	}	
-	else if (fit === "exponential") {
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[2]*Math.E**(coefs[3]*xs[i]) + coefs[4]); 
-		}
-	}
-	else if (fit === "power law") {
-		for (let i = 0; i < xs.length; i++) {
-			if (isWhole(coefs[1])) { //if this coef is a fractional, then we need to exclude negative x values
-				if (xs[i] > 0) {
-					xsToGraph.push(xs[i]);
-					ysToGraph.push(coefs[0]*xs[i]**coefs[1]);
-				}
-			}
-			else {
-				xsToGraph.push(xs[i]);
-				ysToGraph.push(coefs[0]*xs[i]**coefs[1]); 
-			}
-		}
-	}
-	else { //no relation 
-		for (let i = 0; i < xs.length; i++) {
-			xsToGraph.push(xs[i]);
-			ysToGraph.push(coefs[0]); 
-		} 
-	}
-	return [xsToGraph, ysToGraph]; 
+  switch(fit) {
+    case "quadratic":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[0] * xs[i]**2 + coefs[1] * xs[i] + coefs[2]);
+      }
+      break;
+    case  "linear":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[0] * xs[i] + coefs[1]);
+      }
+      break;
+    case "square law":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[0] * xs[i]**2);
+      }
+      break;
+    case "inverse":
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] !== 0) {
+          xsToGraph.push(xs[i]);
+          ysToGraph.push(coefs[0] * (1 / xs[i])); 
+        }
+      }
+      break;
+    case "inverse square":
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] !== 0) {
+          xsToGraph.push(xs[i]);
+          ysToGraph.push(coefs[0] * (1 / xs[i]**2)); 
+        }
+      }
+      break;
+    case "proportional":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[0] * xs[i]);
+      }
+      break;
+    case "square root":
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] >= 0) {
+          xsToGraph.push(xs[i]);
+          ysToGraph.push(coefs[0] * Math.sqrt(xs[i]));
+        }
+      }
+      break;
+    case "exactly proportional":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(xs[i]);
+      }
+      break;
+    case "exponential":
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[2] * Math.E**(coefs[3] * xs[i]) + coefs[4]); 
+      }
+      break;
+    case "power law":
+      for (let i = 0; i < xs.length; i++) {
+        if (isWhole(coefs[1])) { //if this coef is a fractional, then we need to exclude negative x values
+          if (xs[i] > 0) {
+            xsToGraph.push(xs[i]);
+            ysToGraph.push(coefs[0]*xs[i]**coefs[1]);
+          }
+        }
+        else {
+          xsToGraph.push(xs[i]);
+          ysToGraph.push(coefs[0]*xs[i]**coefs[1]); 
+        }
+      }
+      break;
+    default:
+      for (let i = 0; i < xs.length; i++) {
+        xsToGraph.push(xs[i]);
+        ysToGraph.push(coefs[0]); 
+      }
+  }
+  return [xsToGraph, ysToGraph];
 }
 
 function isWhole(num) {
@@ -110,104 +116,109 @@ function isWhole(num) {
 }
 
 function computeRMSE(xs, ys, fit, coefs) {
+  /*
+    computes rmse for points whose x-values are not singularities. 
+    display of rmse for a set of data points whose fit produced one or more singularities
+    is appended with +/- infinity
+  */
 	let sumOfErrSq = 0;
-	let N = xs.length; //for keeping track of singularities
+	let N = xs.length; 
 
-	if (fit === "quadratic") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = coefs[0]*xs[i]**2 + coefs[1]*xs[i] + coefs[2];
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}
-	else if (fit === "linear") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = coefs[0]*x + coefs[1];
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}
-	else if (fit === "square law") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = coefs[0]*xs[i]**2;
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}
-	else if (fit === "inverse") {
-		N = 0;
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] !== 0) {
-				N += 1;
-				yhat = coefs[0]*(1/xs[i]);
-				sumOfErrSq += (ys[i] - yhat)**2;
-			}
-		}
-	}
-	else if (fit === "inverse square") {
-		N = 0;
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] !== 0) {
-				N += 1;
-				yhat = coefs[0]*(1/xs[i]**2); 
-				sumOfErrSq += (ys[i] - yhat)**2;
-			}
-		}
-	}
-	else if (fit === "proportional") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = coefs[0]*xs[i];
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}
-	else if (fit === "square root") {
-		N = 0;
-		for (let i = 0; i < xs.length; i++) {
-			if (xs[i] >= 0) {
-				N += 1;
-				yhat = coefs[0]*Math.sqrt(xs[i]);
-				sumOfErrSq += (ys[i] - yhat)**2;
-			}
-		}
-	}
-	else if (fit === "exactly proportional") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = xs[i];
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}	
-	else if (fit === "exponential") {
-		for (let i = 0; i < xs.length; i++) {
-			yhat = coefs[2]*Math.E**(coefs[3]*xs[i]) + coefs[4]; 
-			sumOfErrSq += (ys[i] - yhat)**2;
-		}
-	}
-	else if (fit === "power law") {
-		N = 0;
-		for (let i = 0; i < xs.length; i++) {
-			if (isWhole(coefs[1])) {
-				if (xs[i] > 0) {
-					N += 1;
-					yhat = coefs[0]*xs[i]**coefs[1];
-					sumOfErrSq += (ys[i] - yhat)**2;
-				}
-			}
-			else {
-				N += 1;
-				yhat = coefs[0]*xs[i]**coefs[1]; 
-				sumOfErrSq += (ys[i] - yhat)**2;
-			}
-		}
-	}
-	else {
-		for (let i = 0; i < xs.length; i++){
-			yhat = coefs[0]; 
-			sumOfErrSq += (ys[i] - yhat)**2;
-		} 
-	}
-
-	let addInf = false;
+  switch(fit) {
+    case "quadratic":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = coefs[0] * xs[i]**2 + coefs[1] * xs[i] + coefs[2];
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "linear":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = coefs[0] * xs[i] + coefs[1];
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "square law":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = coefs[0] * xs[i]**2;
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "inverse":
+      N = 0;
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] !== 0) {
+          N += 1;
+          yhat = coefs[0] * (1 / xs[i]);
+          sumOfErrSq += (ys[i] - yhat)**2;
+        }
+      }
+      break;
+    case "inverse square":
+      N = 0;
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] !== 0) {
+          N += 1;
+          yhat = coefs[0] * (1 / xs[i]**2); 
+          sumOfErrSq += (ys[i] - yhat)**2;
+        }
+      }
+      break;
+    case "proportional":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = coefs[0] * xs[i];
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "square root":
+      N = 0;
+      for (let i = 0; i < xs.length; i++) {
+        if (xs[i] >= 0) {
+          N += 1;
+          yhat = coefs[0] * Math.sqrt(xs[i]);
+          sumOfErrSq += (ys[i] - yhat)**2;
+        }
+      }
+      break;
+    case "exactly proportional":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = xs[i];
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "exponential":
+      for (let i = 0; i < xs.length; i++) {
+        yhat = coefs[2] * Math.E**(coefs[3] * xs[i]) + coefs[4]; 
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+      break;
+    case "power law":
+      N = 0;
+      for (let i = 0; i < xs.length; i++) {
+        if (isWhole(coefs[1])) {
+          if (xs[i] > 0) {
+            N += 1;
+            yhat = coefs[0] * xs[i]**coefs[1];
+            sumOfErrSq += (ys[i] - yhat)**2;
+          }
+        }
+        else {
+          N += 1;
+          yhat = coefs[0] * xs[i]**coefs[1]; 
+          sumOfErrSq += (ys[i] - yhat)**2;
+        }
+      }
+      break;
+    default:
+      for (let i = 0; i < xs.length; i++){
+        yhat = coefs[0]; 
+        sumOfErrSq += (ys[i] - yhat)**2;
+      }
+  }
+  let addInf = false;
 	if (N !== xs.length) {
 		addInf = true;
 	}
-	return [Math.sqrt(sumOfErrSq/N), addInf]; //addInf tells us whether to display RMSE + inf to indicate their were undefined points
+	return [Math.sqrt(sumOfErrSq/N), addInf];
 }
 
 
@@ -215,9 +226,9 @@ function computeRMSE(xs, ys, fit, coefs) {
 
 function reportRMSE(dataObject, fitSelection, coefs, fitToReport) {
 	const RMSE = document.getElementById("rmse");
+  RMSE.innerText = "";
 
 	if (fitToReport) {
-		
 		let [rmse, addInf] = computeRMSE(dataObject.activeX, dataObject.activeY, fitSelection, coefs);
 		
 		if (addInf) {
@@ -226,9 +237,6 @@ function reportRMSE(dataObject, fitSelection, coefs, fitToReport) {
 		else {
 			RMSE.innerText = "RMSE: " + rmse;
 		}
-	}
-	else {
-		RMSE.innerText = "";
 	}
 }
 
