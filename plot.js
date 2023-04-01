aee846bf2c371c2cb26182ae490cd1f2c84790e3
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-01 08:45:57 -0400

    Add functions for reporting fit, rmse

--- plot.js
+++ plot.js
@@ -1,4 +1,16 @@
-// functions for plotting
+// functions for the graph and graph report
+
+let layout = {
+  title: "", //THIS WILL CHANGE = eq from reportRMSE??
+  showlegend: false, 
+  paper_bgcolor: '#edeae5', 
+  plot_bgcolor: '#edeae5', 
+  font: {
+    family: 'Inter, monospace',
+    size: 14,
+    color: '#026670'
+  } 
+};
 
 //THIS AND RMSE CAN BE SWITCH STATEMENTS
 //solving for y, for plotting regression line
@@ -196,4 +208,121 @@ function computeRMSE(xs, ys, fit, coefs) {
 		addInf = true;
 	}
 	return [Math.sqrt(sumOfErrSq/N), addInf]; //addInf tells us whether to display RMSE + inf to indicate their were undefined points
+}
+
+
+//BELOW WILL NEED ADJUSTMENTS...
+
+function reportRMSE(dataObject, fitSelection, coefs, fitToReport) {
+	const RMSE = document.getElementById("rmse");
+
+	if (fitToReport) {
+		
+		let [rmse, addInf] = computeRMSE(dataObject.activeX, dataObject.activeY, fitSelection, coefs);
+		
+		if (addInf) {
+			RMSE.innerText = "RMSE: " + rmse + " + " + "&infin;";
+		}
+		else {
+			RMSE.innerText = "RMSE: " + rmse;
+		}
+	}
+	else {
+		RMSE.innerText = "";
+	}
+}
+
+function addNotEnoughDataWarning(fitSelection) {
+	const fitReport = document.getElementById("coefs"); 
+	fitReport.innerText = "not enough data for " + fitSelection + " fit"; 
+}
+
+function reportFit(fit, coefs, covar) {
+	const eqParagraph = document.getElementById("equation");	//NEED TO MAKE SURE JS HAS A PLACE FOR THESE THINGS
+	const coefParagraph = document.getElementById("coefs");
+
+	if (fit === "quadratic") {
+		const eq = "y = Ax" + String.fromCharCode(178) + " + Bx + C"; 
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+		const b = "B = " + coefs[1] + " " + "&plusmn;" + " " + Math.sqrt(covar[1][1]); 
+		const c = "C = " + coefs[2] + " " + "&plusmn;" + " " + Math.sqrt(covar[2][2]);
+		
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a + "\n" + b + "\n" + c; 
+	}
+	else if (fit === "linear") {
+		const eq = "y = Ax + B";
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+		const b = "B = " + coefs[1] + " " + "&plusmn;" + " " + Math.sqrt(covar[1][1]); 
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a + "\n" + b;
+	}
+	else if (fit === "square law") {
+		const eq = "y = Ax" + String.fromCharCode(178);
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}
+	else if (fit === "inverse") {
+		const eq = "y = A/x";
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}
+	else if (fit === "inverse square") {
+		const eq = "y = A/x" + String.fromCharCode(178);
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}
+	else if (fit === "proportional") {
+		const eq = "y = Ax";
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}
+	else if (fit === "exactly proportional") {
+		const eq = "y = x";
+		eqParagraph.innerText = eq;
+	}
+	else if (fit === "square root") {
+		const eq = "y = A" + "\u221A" + "x"; 	
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}	
+	else if (fit === "exponential") {
+		const eq = "y = Ae" +  "\u1D2D\u02E3"+ " + C"; 
+		const a = "A = " + coefs[2] + " " + "&plusmn;" + " " + Math.sqrt(covar[2][2]);
+		const b = "B = " + coefs[3] + " " + "&plusmn;" + " " + Math.sqrt(covar[3][3]); 
+		const c = "C = " + coefs[4] + " " + "&plusmn;" + " " + Math.sqrt(covar[4][4]);
+		
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a + "\n" + b + "\n" + c; 
+	}
+	else if (fit === "power law") {
+		const eq = "y = Ax" + "\u1D47"; // should be: supescript b
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+		const b = "B = " + coefs[1] + " " + "&plusmn;" + " " + Math.sqrt(covar[1][1]); 
+		
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a + "\n" + b;  
+	}
+	else if (fit === "no relation") {
+		const eq = "y = A";
+		const a = "A = " + coefs[0] + " " + "&plusmn;" + " " + Math.sqrt(covar[0][0]);
+
+		eqParagraph.innerText = eq;
+		coefParagraph.innerText = a;
+	}
+	else {
+		eqParagraph.innerText = "";
+		coefParagraph.innerText = "";
+	}
 }
