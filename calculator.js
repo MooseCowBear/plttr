951bb58e219b037b4866cff2f24607a3ebd286ee
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-04 18:13:18 -0400

    Add refactored functions for adding user input to formula

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 /* functions relating to adding/ evaluating formulas */
 
-function resetModal() {
+function resetModal(formulaState) {
   /*
     function to clear out any changes made to the modal. 
     called whenever the modal closes. 
@@ -18,11 +18,11 @@ function resetModal() {
   const warning = document.querySelector(".invalid-formula");
 	warning.style.visibility = "hidden"; 
 
-	newFormula = ""; //stays
-	infix.length = 0; 
-	prevNum = false;
-	number = "";
-	ROC = 0;
+	formulaState.newFormula = ""; //stays
+	formulaState.infix.length = 0; 
+	formulaState.prevNum = false;
+	formulaState.number = "";
+	formulaState.ROC = 0;
 
 	updateDisplay();
 	enableNonColButtons();
@@ -46,4 +46,74 @@ function enableNonColButtons(){
 	  elem.classList.add("active"); 
   });
 }
+ 
 
+function updateNumber() {
+	/* 
+		called when a non-digit calculator is pushed.
+	*/
+	if (prevNum) { 
+		infix.push(number);
+		prevNum = false;
+		number = ""; 
+	}
+}
+
+function getColNameFromIndex(index) {
+  const the_table = document.getElementById("table");
+	return the_table.rows[0].cells[index].innerText; 
+}
+
+/* reworking all of the functions used for clicking on a calculator button.
+	will add one click event listener to the calculator */
+	
+function cancelFormula() {
+	const formulaModal = document.getElementById("formulaModal");
+	resetModal();
+	formulaModal.style.display = "none";
+}
+
+//everything that is not a digit or roc, updates same...
+
+function addNonDigit(char, formulaState) {
+	/* this includes e and pi, but not functions */
+	warning.style.visibility = "hidden"; 
+	formulaState.newFormula += char;
+	updateDisplay();
+	updateNumber();
+	formulaState.infix.push(char);
+}
+
+function addDigit(digit, formulaState) { 
+	/* decimal counts as digit for our purposes */
+	warning.style.visibility = "hidden"; 
+	formulaState.newFormula += digit;	
+	updateDisplay();
+	formulaState.prevNum = true;
+	formulaState.number += digit;
+}
+
+function addFunction(funcName, charRepresentation, formulaState, withParen = false) {
+	/* every function except for negation adds left parenthesis */
+	warning.style.visibility = "hidden"; 
+	formulaState.newFormula += charRepresentation; 
+	updateDisplay();
+	updateNumber();
+	formulaState.infix.push(funcName);
+	if (withParen) {
+		formulaState.infix.push("(");
+	}
+}
+	
+function addSlope(formulaState) {
+	warning.style.visibility = "hidden"; 
+	formulaState.newFormula += "Rate of Change( , )";  
+	updateDisplay();
+	updateNumber();
+	formulaState.infix.push("slope"); 
+	formulaState.infix.push("(");
+	formulaState.ROC = 1;
+
+	disableNonColButtons();
+}
+	
