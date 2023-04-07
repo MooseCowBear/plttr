c5334eb6f477edb823305fb494bd6f8ddf7d82bf
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-07 12:31:53 -0400

    Add event listeners for graph selections, clear graph button, clear everything button, closing calculator modal when clicking outside it. Remove dummy graph

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     xAxisError: null,
     yAxis: null,
     yAxisError: null,
-  }
+  };
 
   const formulaState = {
 		infix: [],
@@ -28,10 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const yColumn = document.getElementById("y-col");
 
   xColumn.addEventListener("blur", () => {
-    updateColumnName();
+    updateColumnName(xColumn);
   });
   yColumn.addEventListener("blur", () => {
-    updateColumnName(false);
+    updateColumnName(yColumn, false);
   });
 
   //event listener for data being added to the table
@@ -49,78 +49,40 @@ document.addEventListener('DOMContentLoaded', () => {
     //close  x was clicked
     else if (event.target.id === "close") {
       const formulaModal = document.getElementById("formula-modal");
-      resetModal();
+      resetModal(formulaState);
   	  formulaModal.style.display = "none";
     }
   });
 
-  //event listener to close modal if click outside the calculator
+  //for closing the calculator by clicking outside it
+  window.addEventListener('click', function(event) {
+    const formulaModal = document.getElementById("formula-modal");
+    if (event.target === formulaModal) {
+      resetModal(formulaState);
+      formulaModal.style.display = "none";
+      }
+  });
 
-  //graphing event listeners
+  //graphing event listener
+  const graphSelectionDiv = document.querySelector(".selectors");
+  graphSelectionDiv.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      event.target.parentNode.nextElementSibling.classList.toggle("show");  
+      event.target.nextElementSibling.classList.toggle("show");
+    }
+    addSelection(event, graphState); //can move if button out here..
+
+  });
 
   //clear graph event listener
+  const clearGraph = document.getElementById("clear-graph");
+  clearGraph.addEventListener("click", () => {
+    hideGraph(graphState);
+  });
 
   //clear everything event listener
+  const clearEverything = document.getElementById("clear-everything");
+  clearEverything.addEventListener("click", () => {
+    window.location.reload();
+  }); 
 });
-
-
-//some testing to see what the graph will look like... 
-
-let exp = "Math.sin(x)"; 
-
-// Generate values
-let xValues = [];
-let yValues = [];
-
-for (let x = 0; x <= 10; x += 0.1) {  //100-ish data points for regression line
-  yValues.push(Math.sin(x));
-  xValues.push(x);
-}
-
-console.log("the number of x vals is: ", xValues.length); 
-
-// Display using Plotly
-
-let layout = {title: "y = " + exp, showlegend: false, paper_bgcolor: '#edeae5', plot_bgcolor: '#edeae5', font: {
-  family: 'Inter, monospace',
-  size: 14,
-  color: '#026670'
-} };
-
-let points = {
-  x: [0, 1, 2, 3, 4, 5], 
-  y: [1.5, 1, 1.3, 0.7, 0.8, 0.9], 
-  error_x: {
-    type: 'data',
-    array: [0, 0, 0, 0, 0, 0],
-    visible: false
-  },
-  marker:{
-    color: 'hsla(185, 96%, 22%, 0.7)'
-  },
-  mode: 'markers',
-  type: 'scatter'
-};
-
-let line = {
-  x:xValues, y:yValues, marker: { color: '#026670'}, mode:"lines" //"lines is what connects the points"
-}
-
-const graph = document.getElementById("graph");
-
-let config = {responsive: true}
-let data = [points, line]
-Plotly.newPlot("graph", data, layout, config);
-
-
-//TEST! - this moves into dom loaded event
-const clearEverything = document.getElementById("clear-everything");
-clearEverything.addEventListener("click", refresh); 
-
-function refresh() {
-  window.location.reload(); 
-
-	console.log(formulaState); //FOR DEBUGGING
-  console.log(graphState);
-	console.log("formulas", formulas.entries()); 
-}
